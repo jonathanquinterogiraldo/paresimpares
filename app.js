2047207242fefb6597f933bc371e84980014a71a
Jonathan Quintero Giraldo <jonathan.quintero@newshore.es>
2021-05-13 19:51:54 -0500

    app ok

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+let string = '';
+
+app.get('/', (req, res) => {  
+
+     for ( let i = 1; i <= 50; i++){
+         if ( i % 2 === 0){
+            string  += `<p>  ${i} Soy Par!</p> `;
+         }else{
+            string  += `<p>  ${i} Soy Impar!</p> `;
+         }
+        
+    }
+
+    res.send(
+        `<!DOCTYPE html>
+            <head>   
+            </head>
+            <body>
+            ${string}             
+            </body>
+            </html>`);             
+  
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
